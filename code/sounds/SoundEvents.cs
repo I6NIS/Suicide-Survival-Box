a1f79a5e5bc7c3a6c6a60625145172d5d0c55195
YWillems <Yannick.willems@vdabcampus.be>
2021-10-29 03:05:29 +0200

    messing with sound events

---
+++ code/sounds/SoundEvents.cs
@@ -0,0 +1,14 @@
+﻿using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuicideSurvival.sounds
+{
+	class SoundEvents
+	{
+		static SoundEvent ShrubImpact = new SoundEvent( "sound/weapons/bolt_smack1.vsnd_c" );
+	}
+}

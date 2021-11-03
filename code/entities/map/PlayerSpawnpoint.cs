48f9ec9b76b4cc13a8ac92d27a526106dff288e7
I6NIS <ignis1992@gmail.com>
2021-11-03 22:14:32 +0100

    Added player spawnpoint things

---
+++ code/entities/map/PlayerSpawnpoint.cs
@@ -0,0 +1,19 @@
+﻿using Sandbox;
+using SuicideSurvival.systems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuicideSurvival.entities.map
+{
+	[Library( "ss_spawnpoint")]
+	[Hammer.EditorModel("models/editor/playerstart.vmdl", FixedBounds = true)]
+	[Hammer.EntityTool( "Player Spawnpoint", "Suicide Survival", "Defines a point where players on a team can spawn")]
+	
+	public partial class PlayerSpawnpoint : Entity
+	{
+		[Property] public Team Team { get; set; }
+	}
+}

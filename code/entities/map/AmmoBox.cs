fae638840014a1ac0578c36fc4a9d95d85fd99b1
I6NIS <ignis1992@gmail.com>
2021-11-05 15:33:51 +0100

    experimenting with ammo boxes

---
+++ code/entities/map/AmmoBox.cs
@@ -0,0 +1,17 @@
+﻿using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuicideSurvival.entities.map
+{
+	[Library( "ss_ammo" )]
+	[Hammer.EditorModel( "models/props/ammo_box.vmdl", FixedBounds = true )]
+	[Hammer.EntityTool( "Ammo Box", "Suicide Survival", "Provides ammo for survivors" )]
+	public partial class AmmoBox : Entity
+	{
+
+	}
+}
